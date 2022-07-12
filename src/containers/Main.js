4cc30f730ff6e780fba8b8cb89694d9e2a0a557b
Steve Gossip <stephengossip@gmail.com>
2022-07-12 23:22:25 +0100

    altered the getRandomCocktailData function to include all relevant details from the API. Created 2 lists from the data for ingredients and measures and then setState for all the new data to be included.

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,6 @@
 import Cocktail from '../components/Cocktail';
 import CocktailBySpiritForm from '../components/CocktailBySpiritForm';
+import DrinkDetails from '../components/DrinkDetails';
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -16,10 +17,39 @@ export default function Main() {
       .then((response) => {
         const res = response.data;
         const random_cocktail = res['drinks'][0]
-        console.log(res)
+        // sort ingredients into a list
+        const ingredients = []
+        for (const [key,value] of Object.entries(random_cocktail)) {
+          if (key.includes("strIngredient")) {
+            if (value !== null) {
+              let new_object = {};
+              new_object[key] = value;
+              ingredients.push(new_object)
+            }
+          }
+        }
+        console.log("Ingredients: ",ingredients)
+        // sort measures into a list
+        const measures = []
+        for (const [key,value] of Object.entries(random_cocktail)) {
+          if (key.includes("strMeasure")) {
+            if (value !== null) {
+              let new_object = {};
+              new_object[key] = value;
+              measures.push(new_object)
+            }
+          }
+        }
+        console.log("measures: ",measures)
         setCurrentCocktailData({
+          id: random_cocktail.idDrink,
           name: random_cocktail.strDrink,
+          glass: random_cocktail.strGlass,
+          ingredients: ingredients,
+          measures: measures,
+          instructions: random_cocktail.strInstructions,
           image: random_cocktail.strDrinkThumb
+
         })
       })
       .catch((error) => {
@@ -38,7 +68,7 @@ export default function Main() {
     .then((response) => {
       const res = response.data;
       const cocktail = res['drinks'][0]
-      console.log(res)
+      console.log(cocktail)
       setCurrentCocktailData({
         name: cocktail.strDrink,
         image: cocktail.strDrinkThumb
@@ -53,8 +83,9 @@ export default function Main() {
 
   return (
     <React.Fragment>
-      <CocktailBySpiritForm cocktailBySpirit={currentCocktailData} getRandomCocktail={getRandomCocktailData} getCocktailBySpirit={getCocktailBySpirit} />
+      <CocktailBySpiritForm cocktailBySpirit={currentCocktailData} getCocktail={getRandomCocktailData} getCocktailBySpirit={getCocktailBySpirit} />
       <Cocktail cocktail={currentCocktailData} />
+      <DrinkDetails cocktail={currentCocktailData} />
     </ React.Fragment>
   )
 }
