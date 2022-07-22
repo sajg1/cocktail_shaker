4e5ce9e923878666e5d8d192e23789fcd575e017
Steve Gossip <stephengossip@gmail.com>
2022-07-22 23:25:20 +0100

    updated LikedCocktails.js to show a list of all the cocktail names that are in the database.

--- src/components/LikedCocktails.js
+++ src/components/LikedCocktails.js
@@ -3,14 +3,16 @@ import React from 'react';
 const LikedCocktails = (props) => {
   return(
     <div>
-      <div>
-        <h2>List of your liked Cocktails</h2>
-        <ul>
-          <li>This is a list of Cocktails</li>
-        </ul>
-      </div>
+      <h2>List of your liked Cocktails</h2>
+      { props.liked &&
+        <div>
+          <ul>
+            {props.liked.map((cocktail, index) => <li key={index}>{cocktail.name}</li>)}
+          <ul>
+        </div>
+      }
     </div>
   )
 }
 
-export default LikedCocktails
+export default LikedCocktails;
